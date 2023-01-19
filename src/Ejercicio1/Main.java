d210152ed8da9351a44ef76a5fe0ed932b8ea83f
Nagore Sanchez <Nagore Sanchez@HZ114832.H012108.NET>
2023-01-19 11:49:35 +0100

    commit

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -1,5 +1,16 @@
 package Ejercicio1;
 
+import java.sql.SQLException;
+
 public class Main {
+
+	
+
+		public static void main(String[] args) throws ClassNotFoundException, SQLException {
+			GestorArboles inicializador = new GestorArboles();
+			inicializador.run();
+		
+		
+	}
 
 }
