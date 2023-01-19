756e7945984a2934aeda3e52f7de064c118c1420
Nagore Sanchez <Nagore Sanchez@HZ114832.H012108.NET>
2023-01-19 11:49:53 +0100

    getter setters hecho y constructores

---
+++ src/Ejercicio1/Arbol.java
@@ -0,0 +1,79 @@
+package Ejercicio1;
+
+public class Arbol {
+
+	private int id;
+	private String nombreComun;
+	private String nombreCientifico;
+	private String habitat;
+	private int altura;
+	private String origen;
+	
+	
+	public Arbol() {
+		//sin parametros
+	}
+	
+	public Arbol(int id, String nombreComun, String nombreCientifico, String habitat, int altura, String origen ) {
+		this.id=id;
+		this.nombreComun=nombreComun;
+		this.nombreCientifico=nombreCientifico;
+		this.habitat=habitat;
+		this.altura=altura;
+		this.origen=origen;
+		//con parametros
+	}
+
+	
+	//get y set
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombreComun() {
+		return nombreComun;
+	}
+
+	public void setNombreComun(String nombreComun) {
+		this.nombreComun = nombreComun;
+	}
+
+	public String getNombreCientifico() {
+		return nombreCientifico;
+	}
+
+	public void setNombreCientifico(String nombreCientifico) {
+		this.nombreCientifico = nombreCientifico;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	//fin get y set
+	
+	
+}
